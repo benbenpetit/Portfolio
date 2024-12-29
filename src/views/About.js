7aad1aa027526856eb0ddd0d113ec66ca2fddaf0
Benoit PETIT <petitbenoit3@gmail.com>
2024-12-29 15:25:20 +0100

    modify projects

--- src/views/About.js
+++ src/views/About.js
@@ -1,64 +1,133 @@
-import RevealLetters from 'Components/RevealLetters';
-import React, { useRef } from 'react';
-import LocoScroll from 'Utils/LocoScroll';
-import { getRandomNumber } from 'Utils/Math';
+import RevealLetters from 'Components/RevealLetters'
+import React, { useRef } from 'react'
+import LocoScroll from 'Utils/LocoScroll'
+import { getRandomNumber } from 'Utils/Math'
 
 const About = () => {
-  const containerRef = useRef(null);
+  const containerRef = useRef(null)
 
   return (
-    <LocoScroll innerRef={containerRef} direction="horizontal">
-      <main className="about-section" data-scroll-container ref={containerRef}>
-        <section className="about-section__first o-horizontal" data-scroll-section>
-          <h1 className="about-section__title" data-scroll data-scroll-speed="-1">About me</h1>
-          <div className="u-relative">
-            <div className="c-hide-block" data-scroll data-scroll-offset="0, -1000%">
-              {"interested in my profile ?".split(' ').map((word, i) =>
-                <p key={i} className="c-hide-block__paragraph" data-scroll data-scroll-speed={getRandomNumber(3, true).toString()}>
-                  <span className="c-hide-block__hide-wrapper">
-                    <span className={`c-hide-block__hide-wrapper__inside -delay-${i * 2 + 13}`}>{word}</span>
+    <LocoScroll innerRef={containerRef} direction='horizontal'>
+      <main className='about-section' data-scroll-container ref={containerRef}>
+        <section
+          className='about-section__first o-horizontal'
+          data-scroll-section
+        >
+          <h1
+            className='about-section__title'
+            data-scroll
+            data-scroll-speed='-1'
+          >
+            About me
+          </h1>
+          <div className='u-relative'>
+            <div
+              className='c-hide-block'
+              data-scroll
+              data-scroll-offset='0, -1000%'
+            >
+              {'interested in my profile ?'.split(' ').map((word, i) => (
+                <p
+                  key={i}
+                  className='c-hide-block__paragraph'
+                  data-scroll
+                  data-scroll-speed={getRandomNumber(3, true).toString()}
+                >
+                  <span className='c-hide-block__hide-wrapper'>
+                    <span
+                      className={`c-hide-block__hide-wrapper__inside -delay-${
+                        i * 2 + 13
+                      }`}
+                    >
+                      {word}
+                    </span>
                   </span>
                 </p>
-              )}
+              ))}
             </div>
-            <div className="about-section__maxi-arrow" data-scroll data-scroll-repeat="false">
-              <svg viewBox="0 0 285.4 185.2">
-                <polyline points="147,2.2 281,92.7 147,183.2 "></polyline>
-                <line x1="281" y1="92.7" x2="1.4" y2="92.4"></line>
+            <div
+              className='about-section__maxi-arrow'
+              data-scroll
+              data-scroll-repeat='false'
+            >
+              <svg viewBox='0 0 285.4 185.2'>
+                <polyline points='147,2.2 281,92.7 147,183.2 '></polyline>
+                <line x1='281' y1='92.7' x2='1.4' y2='92.4'></line>
               </svg>
             </div>
           </div>
         </section>
-        <section className="about-section__second o-horizontal" data-scroll-section>
-          <div className="about-section__container u-anim" data-scroll data-scroll-offset="35%, -1000%">
-            <div className="about-section__presentation">
-              <div className="about-section__presentation__left">
-                <h2 className="about-section__presentation__left__title c-reveal-letters" data-scroll data-scroll-offset="35%, -1000%">
-                  <RevealLetters text="My story" />
+        <section
+          className='about-section__second o-horizontal'
+          data-scroll-section
+        >
+          <div
+            className='about-section__container u-anim'
+            data-scroll
+            data-scroll-offset='35%, -1000%'
+          >
+            <div className='about-section__presentation'>
+              <div className='about-section__presentation__left'>
+                <h2
+                  className='about-section__presentation__left__title c-reveal-letters'
+                  data-scroll
+                  data-scroll-offset='35%, -1000%'
+                >
+                  <RevealLetters text='My story' />
                 </h2>
-                <img className="about-section__presentation__left__pic" src={process.env.PUBLIC_URL + '/img/me.jpg'} alt="Face of Benoît Petit" data-scroll data-scroll-speed="-2" />
+                <img
+                  className='about-section__presentation__left__pic'
+                  src={process.env.PUBLIC_URL + '/img/me.jpg'}
+                  alt='Face of Benoît Petit'
+                  data-scroll
+                  data-scroll-speed='-2'
+                />
               </div>
-              <div className="about-section__presentation__right">
-                <h3 className="about-section__presentation__right__title">Hey,</h3>
-                <div className="about-section__presentation__right__paragraphs o-block">
-                  <p>I am 20 years old and i live next to Paris</p>
-                  <p>After a DUT MMI at Troyes and a Professional License web at Sorbonne University, I am now trying to integrate Les Gobelins school!</p>
+              <div className='about-section__presentation__right'>
+                <h3 className='about-section__presentation__right__title'>
+                  Hey,
+                </h3>
+                <div className='about-section__presentation__right__paragraphs o-block'>
+                  <p>I am 22 years old and i live next to Paris</p>
+                  <p>
+                    After a DUT MMI at Troyes and a Professional License web at
+                    Sorbonne University, I have just finished a master's degree
+                    Design and Management of Interactive Innovation at Les
+                    Gobelins school!
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <section className="about-section__third o-horizontal" data-scroll-section>
-          <div className="about-section__container u-anim" data-scroll>
-            <h2 className="about-section__title c-reveal-letters" data-scroll>
-              <RevealLetters text="Contact" />
+        <section
+          className='about-section__third o-horizontal'
+          data-scroll-section
+        >
+          <div className='about-section__container u-anim' data-scroll>
+            <h2 className='about-section__title c-reveal-letters' data-scroll>
+              <RevealLetters text='Contact' />
             </h2>
-            <p className="about-section__paragraph o-block">Contact me through the (soon fonctionnal 🛠) form on the website or by clicking on email <a href="mailto:petitbenoit3@gmail.com">petitbenoit3@gmail.com</a></p>
+            <p className='about-section__paragraph o-block'>
+              Contact me by sending me an email (
+              <a href='mailto:petitbenoit3@gmail.com'>petitbenoit3@gmail.com</a>
+              ) or{' '}
+              <a
+                href='https://www.linkedin.com/in/benbenpetit/'
+                target='_blank'
+              >
+                LinkedIn
+              </a>{' '}
+              or{' '}
+              <a href='https://github.com/benbenpetit' target='_blank'>
+                Github
+              </a>
+            </p>
           </div>
         </section>
       </main>
     </LocoScroll>
-  );
-};
+  )
+}
 
-export default About;
+export default About
--- src/views/CaseStudy.js
+++ src/views/CaseStudy.js
@@ -1,81 +1,250 @@
-import React, { useEffect, useRef } from 'react';
-import { useParams } from "react-router";
-import { useNavigate } from "react-router-dom";
-import workBlocks from "Data/works.json";
-import Arche from 'Components/Arche';
-import Link from 'Components/Link';
-import HideBlock from 'Components/HideBlock';
-import LineBreak from 'Components/LineBreak';
-import RoundedBackground from 'Components/RoundedBackground';
-import LocoScroll from 'Utils/LocoScroll';
+import React, { useEffect, useRef } from 'react'
+import { useParams } from 'react-router'
+import { useNavigate } from 'react-router-dom'
+import workBlocks from 'Data/works.json'
+import Arche from 'Components/Arche'
+import Link from 'Components/Link'
+import HideBlock from 'Components/HideBlock'
+import LineBreak from 'Components/LineBreak'
+import RoundedBackground from 'Components/RoundedBackground'
+import LocoScroll from 'Utils/LocoScroll'
+
+const CREATIONS = [
+  {
+    title: 'Quadri drag',
+    url: 'https://quadri-drag.vercel.app/',
+    imgPath: 'quadri-drag.jpg'
+  },
+  {
+    title: 'Cute swiper',
+    url: 'https://cute-swiper.vercel.app/',
+    imgPath: 'cute-swiper.jpg'
+  },
+
+  {
+    title: 'Moving images',
+    url: 'https://moving-images.vercel.app/',
+    imgPath: 'moving-images.jpg'
+  },
+  {
+    title: 'Mon blog accessible',
+    url: 'https://mon-blog-accessible.vercel.app',
+    imgPath: 'mon-blog-accessible.jpg'
+  },
+  {
+    title: 'Gallery follow map',
+    url: 'https://gallery-follow-map.vercel.app',
+    imgPath: 'gallery-follow-map.jpg'
+  },
+  {
+    title: 'Soy Cuba',
+    url: 'https://bromley-and-co.vercel.app/',
+    imgPath: 'soy-cuba.jpg'
+  },
+  {
+    title: 'Watch video',
+    url: 'https://watch-video.vercel.app/',
+    imgPath: 'watch-video.jpg'
+  },
+  {
+    title: 'Oucékecé',
+    url: 'https://oucekece.vercel.app/',
+    imgPath: 'oucekece.jpg'
+  },
+  {
+    title: 'Alpha tango',
+    url: 'https://alpha-tango.vercel.app/',
+    imgPath: 'alpha-tango.jpg'
+  }
+]
 
 const CaseStudy = () => {
-  const { pathname } = useParams();
-  const navigate = useNavigate();
-  const containerRef = useRef(null);
-  const workContent = workBlocks.find(work => work.pagePath === pathname);
+  const { pathname } = useParams()
+  const navigate = useNavigate()
+  const containerRef = useRef(null)
+  const workContent = workBlocks.find((work) => work.pagePath === pathname)
 
   useEffect(() => {
-    if (!workBlocks.some(work => work.pagePath === pathname)) {
-      navigate('/', { replace: true });
+    if (!workBlocks.some((work) => work.pagePath === pathname)) {
+      navigate('/', { replace: true })
     }
-  }, [navigate, pathname]);
+  }, [navigate, pathname])
 
   return (
-    <LocoScroll innerRef={containerRef} direction="vertical">
-      {workContent &&
+    <LocoScroll innerRef={containerRef} direction='vertical'>
+      {workContent && (
         <main data-scroll-container ref={containerRef}>
-          <header className="c-hero-study" data-scroll-section>
-            <div className="c-hero-study__wrapper" data-scroll data-scroll-speed="-2">
+          <header className='c-hero-study' data-scroll-section>
+            <div
+              className='c-hero-study__wrapper'
+              data-scroll
+              data-scroll-speed='-2'
+            >
               <h1>{workContent.title}</h1>
               <h2>{workContent.subtitle}</h2>
             </div>
           </header>
-          <div id="scroll-section">
-            <section className="case-study-section" data-scroll-section>
-              <div className="case-study-section__view-case">
-                <a className="case-study-section__view-case__link" rel="noreferrer" href={workContent.article.demoPath} target="_blank">
-                  <img src={process.env.PUBLIC_URL + '/img/arrow.png'} alt="White arrow" />
-                  <span>view</span>
-                  <span>case</span>
-                </a>
-                <div className="case-study-section__view-case__ovale">
-                  <div className="case-study-section__view-case__ovale__symbol"></div>
-                  <div className="case-study-section__view-case__ovale__symbol"></div>
+          <div id='scroll-section'>
+            <section className='case-study-section' data-scroll-section>
+              {workContent.article.demoPath && (
+                <div className='case-study-section__view-case'>
+                  <a
+                    className='case-study-section__view-case__link'
+                    rel='noreferrer'
+                    href={workContent.article.demoPath}
+                    target='_blank'
+                  >
+                    <img
+                      src={process.env.PUBLIC_URL + '/img/arrow.png'}
+                      alt='White arrow'
+                    />
+                    <span>view</span>
+                    <span>case</span>
+                  </a>
+                  <div className='case-study-section__view-case__ovale'>
+                    <div className='case-study-section__view-case__ovale__symbol'></div>
+                    <div className='case-study-section__view-case__ovale__symbol'></div>
+                  </div>
                 </div>
-              </div>
-              <div className="case-study-section__article">
-                <header className="case-study-section__article__header u-anim" data-scroll data-scroll-offset="20%" data-scroll-id="case-study-img">
-                  <Arche src={process.env.PUBLIC_URL + '/img/' + workContent.article.images[0]} alt="Page d'accueil de Spotify" />
+              )}
+              <div className='case-study-section__article'>
+                <header
+                  className='case-study-section__article__header u-anim'
+                  data-scroll
+                  data-scroll-offset='20%'
+                  data-scroll-id='case-study-img'
+                >
+                  <Arche
+                    src={
+                      process.env.PUBLIC_URL +
+                      '/img/' +
+                      workContent.article.images[0]
+                    }
+                  />
                 </header>
-                <div className="case-study-section__article__content">
+                <div className='case-study-section__article__content'>
                   <LineBreak />
                   <HideBlock>{workContent.article.text[0]}</HideBlock>
-                  <div className="o-block">
-                    <div className="o-block o-block--image" tabIndex="0">
-                      <img src={process.env.PUBLIC_URL + '/img/' + workContent.article.images[1]} alt="Spotify replica card" />
-                    </div>
-                    <p className="o-block o-block--text">{workContent.article.text[1]}</p>
-                  </div>
-                  <LineBreak />
-                  <HideBlock>{workContent.article.text[2]}</HideBlock>
-                  <div className="c-underline-explanation" data-scroll data-scroll-offset="0, -1000%">
-                    <div className="o-block o-block--image" tabIndex="0">
-                      <img src={process.env.PUBLIC_URL + '/img/' + workContent.article.images[2]} alt="Spotify replica card" />
-                    </div>
-                  </div>
-                  <footer className="case-study-section__article__content__footer">
-                    <Link path="/">
-                      <div className="c-return-button" data-scroll>
-                        <img className="c-return-button__arrow" src={process.env.PUBLIC_URL + '/img/arrow.png'} alt="White arrow" />
-                        <svg className="c-return-button__svg" viewBox="0 0 120 120">
-                          <circle cx="60" cy="60" r="58"></circle>
+                  {!workContent.article.isTests ? (
+                    <>
+                      <div className='o-block'>
+                        <div className='o-block o-block--image' tabIndex='0'>
+                          <img
+                            src={
+                              process.env.PUBLIC_URL +
+                              '/img/' +
+                              workContent.article.images[1]
+                            }
+                          />
+                        </div>
+                        <p className='o-block o-block--text'>
+                          {workContent.article.text[1]}
+                        </p>
+                      </div>
+                      <LineBreak />
+                      <HideBlock>{workContent.article.text[2]}</HideBlock>
+                      <div
+                        className='c-underline-explanation'
+                        data-scroll
+                        data-scroll-offset='0, -1000%'
+                      >
+                        <div className='o-block o-block--image' tabIndex='0'>
+                          <img
+                            src={
+                              process.env.PUBLIC_URL +
+                              '/img/' +
+                              workContent.article.images[2]
+                            }
+                          />
+                        </div>
+                      </div>
+                      {workContent.article.images[3] && (
+                        <div
+                          className='c-underline-explanation'
+                          data-scroll
+                          data-scroll-offset='0, -1000%'
+                        >
+                          <div className='o-block o-block--image' tabIndex='0'>
+                            <img
+                              src={
+                                process.env.PUBLIC_URL +
+                                '/img/' +
+                                workContent.article.images[3]
+                              }
+                            />
+                          </div>
+                        </div>
+                      )}
+                      {workContent.article.images[4] && (
+                        <div
+                          className='c-underline-explanation'
+                          data-scroll
+                          data-scroll-offset='0, -1000%'
+                        >
+                          <div className='o-block o-block--image' tabIndex='0'>
+                            <img
+                              src={
+                                process.env.PUBLIC_URL +
+                                '/img/' +
+                                workContent.article.images[4]
+                              }
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  ) : (
+                    <>
+                      <div className='o-block'>
+                        <p className='o-block o-block--text'>
+                          {workContent.article.text[1]}
+                        </p>
+                      </div>
+                      {CREATIONS.map((creation, index) => (
+                        <div key={index} className='o-block u-mt-6'>
+                          <div className='o-block o-block--image' tabIndex='0'>
+                            <img
+                              src={
+                                process.env.PUBLIC_URL +
+                                '/img/' +
+                                creation.imgPath
+                              }
+                            />
+                          </div>
+                          <p className='o-block o-block--text'>
+                            <span>{creation.title} : </span>
+                            <a
+                              href={creation.url}
+                              target='_blank'
+                              rel='noreferrer'
+                              style={{ textDecoration: 'underline' }}
+                            >
+                              {creation.url}
+                            </a>
+                          </p>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                  <footer className='case-study-section__article__content__footer'>
+                    <Link path='/'>
+                      <div className='c-return-button' data-scroll>
+                        <img
+                          className='c-return-button__arrow'
+                          src={process.env.PUBLIC_URL + '/img/arrow.png'}
+                          alt='White arrow'
+                        />
+                        <svg
+                          className='c-return-button__svg'
+                          viewBox='0 0 120 120'
+                        >
+                          <circle cx='60' cy='60' r='58'></circle>
                         </svg>
                       </div>
                     </Link>
-                    <Link path="/about" className="c-button u-no-mt">
-                      <span className="c-button__inner -visible">About</span>
-                      <span className="c-button__inner -hidden">About</span>
+                    <Link path='/about' className='c-button u-no-mt'>
+                      <span className='c-button__inner -visible'>About</span>
+                      <span className='c-button__inner -hidden'>About</span>
                     </Link>
                   </footer>
                 </div>
@@ -84,9 +253,9 @@ const CaseStudy = () => {
             </section>
           </div>
         </main>
-      }
+      )}
     </LocoScroll>
   )
 }
 
-export default CaseStudy;
+export default CaseStudy
